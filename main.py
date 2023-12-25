cef480ba0add0f721cc45624c1b15489cb5cff9d
TobitVt <tobit.vervat@gmail.com>
2023-12-25 18:20:40 +0200

    use env variables and create Procfile.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -39,10 +39,6 @@ login_manager.init_app(app)
 
 MAIL_ADDRESS = os.environ.get("EMAIL_KEY")
 MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
-#ozxxzabmfozuqvcz
-#os.environ['PASSWORD_KEY']=ozxxzabmfozuqvcz
-
-#print(MAIL_ADDRESS)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -60,7 +56,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -332,4 +328,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
